61fa701d657d9cf77fd6f0e0c8c9560394eef021
kreytos23 <kreytos.pavi10@gmail.com>
2024-05-06 23:44:30 -0600

    request to API

--- ptreact/src/AppItem/AppItem.js
+++ ptreact/src/AppItem/AppItem.js
@@ -2,11 +2,45 @@ import correo from "../images/correopesca.jpg";
 import pesca from "../images/pesca.png";
 import personCandado from "../images/que-es-phishing-pharming.jpg";
 import "./appItem.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { UploadMedia } from "../SubirArchivos/UploadMedia.js";
-
+import React, { useState, useEffect } from "react";
 
 function AppItem(props) {
+  // const [data, setData] = useState("");
+  // const [file, setFile] = useState(null);
+
+  // useEffect(() => {
+  //   if (file !== null) {
+  //     fetchData();
+  //   }
+  // }, [file]);
+
+  // const onFileChange = (event) => {
+  //   setFile(event.target.files[0]);
+  // };
+
+  // const fetchData = async () => {
+  //   try {
+  //     const formData = new FormData();
+  //     formData.append("mbox_file", file);
+
+  //     const response = await fetch(
+  //       "http://phishing-prediction.us-east-2.elasticbeanstalk.com/upload-mbox",
+  //       {
+  //         method: "POST",
+  //         body: formData,
+  //       }
+  //     );
+
+  //     const result = await response.text();
+  //     console.log("Data fetched:", result);
+  //     setData(result);
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //   }
+  // };
+
   return (
     <div className="appItem">
       <span className="appContainer">
@@ -26,14 +60,18 @@ function AppItem(props) {
 
       <span className="appContainer">
         <p>Conoce más sobre como utilizar esta herramienta</p>
-          <Link to="UploadMedia" className="linkdecor">
-            <button
-              className="leemas2">
-              Usa Phishmail
-            </button>
-          </Link>
-          
+        <Link to="UploadMedia" className="linkdecor">
+          <button className="leemas2">Usa Phishmail</button>
+        </Link>
       </span>
+
+      {/* 
+      <div>
+        <h4>Sube tu archivo mbox</h4>
+        <input type="file" onChange={onFileChange} />
+        <p>{data}</p> 
+      </div>
+      */}
     </div>
   );
 }
