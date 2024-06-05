31b1861706e0b07ec7a8df0afb38052b264c826b
kreytos23 <kreytos.pavi10@gmail.com>
2024-06-05 02:50:12 -0600

    Porcentajes rounded in const even

--- src/results/Results.js
+++ src/results/Results.js
@@ -26,7 +26,7 @@ function Results() {
   });
 
   const percentageValid = Math.ceil(numValidEmails*100/dataJson.TotalEmails);
-  const percentagePhishing = Math.ceil(numPhishingEmails*100/dataJson.TotalEmails);
+  const percentagePhishing = Math.floor(numPhishingEmails*100/dataJson.TotalEmails);
 
   return (
     <div className="tablaResponsive">
